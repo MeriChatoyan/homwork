58dd3e5a6ca75161213a58b6f11ac43c1db1a9d1
MeriChatoyan <merichatoyan888@gmail.com>
2025-02-11 11:00:28 +0400

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    int height;
+    std::cout << "Մուտքագրեք եռանկյան բարձրությունը: ";
+    std::cin >> height;
+   for (int i = 1; i <= height; ++i) {
+        for (int j = 0; j < height - i; ++j) {
+            std::cout << " ";
+        }
+        for (int k = 0; k < 2 * i - 1; ++k) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
